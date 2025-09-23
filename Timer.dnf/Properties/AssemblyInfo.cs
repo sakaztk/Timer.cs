a3afaddbfbba497919e66b99e519e4a89e9e222a
Kazutaka Sakoda <kazutaka.sakoda@gmail.com>
2025-09-23 17:43:52 +0900

    Add Timer.dnf

---
+++ Timer.dnf/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Timer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Timer")]
+[assembly: AssemblyCopyright("Copyright © sakaztk")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c6448e7a-b316-4395-a233-4052cc1ddd5d")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
